54938aa53987c027e3a600f939363524c6abaa7e
harm70 <harm70@users.noreply.github.com>
2014-07-27 09:23:43 +0200

    Update validator.js

--- validator/validator.js
+++ validator/validator.js
@@ -1,1 +1,37 @@
-test 
+var industries = ['','Health', 'Oil', 'IT'];
+
+function validate(form) {
+if (!Modernizr.input.required) {
+var hasErrors = false;
+$("input[required]", form).each(function(){
+if ($(this).val() == "") {
+$(this).css("border", "1px solid red");
+hasErrors = true;
+}
+});
+$("select[required]", form).each(function(){
+if ($(this).val() == "") {
+$(this).css("border", "1px solid red");
+hasErrors = true;
+}
+});
+
+var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+$("input[type=email][required]", form).each(function(){
+var email = $(this).val();
+var isCorrect = regex.test(email);
+if (!isCorrect) {
+$(this).css("border", "1px solid red");
+hasErrors = true;
+}
+});
+}
+showMessage(hasErrors);
+return !hasErrors;
+}
+
+function showMessage(hasErrors) {
+var message = hasErrors == true ? "Invalid value" : "Submit form"
+var clazz = hasErrors == true ? "error" : "success";
+$("#message-container").text(message).addClass(clazz);
+}
